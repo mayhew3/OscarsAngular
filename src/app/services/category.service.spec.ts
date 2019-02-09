b5cace0ca35e97ce5a737edcec337c1bcdbb97f1
Mayhew Seavey <commander.boffo@gmail.com>
2019-02-08 21:10:54 -0800

    MM-6: Add some basic service tests

--- src/app/services/category.service.spec.ts
+++ src/app/services/category.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed, tick} from '@angular/core/testing';
 
 import { CategoryService } from './category.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
@@ -6,6 +6,8 @@ import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDataService} from './in-memory-data-service';
 
 describe('CategoryService', () => {
+  let service: CategoryService;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule,
@@ -14,8 +16,52 @@ describe('CategoryService', () => {
       )]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(CategoryService);
+  });
+
   it('should be created', () => {
-    const service: CategoryService = TestBed.get(CategoryService);
     expect(service).toBeTruthy();
   });
+
+  it('nothing in cache initially', () => {
+    expect(service.cache).toBeTruthy();
+    expect(service.cache.length).toBe(0);
+  });
+
+  it('getCategories puts things in cache', () => {
+    service.getCategories().subscribe((categories) => {
+      expect(service.cache.length).toBeGreaterThan(0);
+      expect(categories.length).toBeGreaterThan(0);
+      expect(categories.length).toBe(service.cache.length);
+      expect(categories.length).toBe(3);
+    });
+  });
+
+  it('getCategory puts things in cache', () => {
+    service.getCategory(2).subscribe((category) => {
+      expect(category.id).toBe(2);
+    });
+  });
+
+  it('nothing in cache initially', () => {
+
+  });
+
+  it('nothing in cache initially', () => {
+
+  });
+
+  it('nothing in cache initially', () => {
+
+  });
+
+  it('nothing in cache initially', () => {
+
+  });
+
+  it('nothing in cache initially', () => {
+
+  });
+
 });
