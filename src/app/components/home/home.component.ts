d770be1c18897dca5dbe471b66826a52d6e22c4a
mayhew3 <commander.boffo@gmail.com>
2019-02-16 12:57:21 -0800

    MA-31: Fix prod build

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import {AuthService} from '../../services/auth/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
