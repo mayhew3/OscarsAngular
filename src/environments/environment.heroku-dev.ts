ea5d1d38e4e8aa4c76fcbdf00da8a4a69aeff567
mayhew3 <commander.boffo@gmail.com>
2019-02-18 13:54:07 -0800

    MA-23: HTTPS

--- src/environments/environment.heroku-dev.ts
+++ src/environments/environment.heroku-dev.ts
@@ -4,7 +4,7 @@ import {EnvironmentConfig} from './environment.interface';
 export const environment: EnvironmentConfig = {
   production: false,
   httpModules: [HttpClientModule],
-  authCallbackUrl: 'http://oscars-vote.herokuapp.com/callback',
+  authCallbackUrl: 'https://oscars-vote.herokuapp.com/callback',
   clientID: 'Re282m5GM0575vOJjhpguBptT8slmIb0',
   domain: 'mayhew3.auth0.com'
 };
