b2fbe0cb31dbadbbcdf12d812e488ce672e60b69
mayhew3 <commander.boffo@gmail.com>
2020-01-17 19:27:01 -0800

    MA-89: Receive message from socket server at web client, and update the odds. It's pretty zippy!

--- src/app/services/odds.service.ts
+++ src/app/services/odds.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, Subscription, timer} from 'rxjs';
 import {OddsBundle} from '../interfaces/OddsBundle';
+import {Socket} from 'ngx-socket-io';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,13 @@ export class OddsService {
   private odds: OddsBundle;
   private eventSubscription: Subscription;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private socket: Socket) {
     this.oddsFirstUpdate().subscribe(odds => {
       this.odds = odds;
+      this.socket.on('odds', msg => {
+        this.odds = msg;
+      });
     });
   }
 
