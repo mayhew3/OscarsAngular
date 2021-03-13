6d1f8086add014593fd4be93cd573852935650ea
mayhew3 <commander.boffo@gmail.com>
2021-03-13 11:28:57 -0800

    Fix SSL

--- controllers/sequelize.js
+++ controllers/sequelize.js
@@ -3,11 +3,18 @@ let config = process.env.DATABASE_URL;
 
 exports.sequelize = new Sequelize(config, {
   dialect: 'postgres',
+  ssl: true,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 });
 
