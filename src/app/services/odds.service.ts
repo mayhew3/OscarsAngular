07091e6cfa52b66c4c6111a3f1eaa8db2e18805e
Mayhew Seavey <commander.boffo@gmail.com>
2021-04-06 00:53:51 -0700

    Remove intentional error

--- src/app/services/odds.service.ts
+++ src/app/services/odds.service.ts
@@ -29,7 +29,7 @@ export class OddsService {
               private socket: SocketService,
               private store: Store,
               private auth: MyAuthService) {
-    this.store.dispatch(new GetOdds());
+    this.auth.isAuthenticated$.subscribe(() => this.store.dispatch(new GetOdds()));
   }
 
 }
