319825f9151840f00459b3d49bf39c9eb416c46b
Mayhew Seavey <commander.boffo@gmail.com>
2021-04-07 16:02:28 -0700

    Add queuing for error notifications

--- src/app/services/error-notification.service.ts
+++ src/app/services/error-notification.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {Observable, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {MatSnackBar, MatSnackBarRef} from '@angular/material/snack-bar';
+import {Observable, of, Subject, Subscription} from 'rxjs';
 import {ErrorNotificationComponent} from '../components/error-notification/error-notification.component';
 
 @Injectable({
@@ -8,7 +8,22 @@ import {ErrorNotificationComponent} from '../components/error-notification/error
 })
 export class ErrorNotificationService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  private messageQueue = new Subject<SnackBarMessage>();
+  private subscription: Subscription;
+  private snackBarRef: MatSnackBarRef<ErrorNotificationComponent>;
+
+  constructor(private snackBar: MatSnackBar) {
+    this.subscription = this.messageQueue.subscribe(snackBarMessage => {
+      if (!this.snackBarRef) {
+        this.openSnackBar(snackBarMessage);
+      } else {
+        this.snackBarRef.afterDismissed().subscribe(() => {
+          this.snackBarRef = undefined;
+          this.openSnackBar(snackBarMessage);
+        });
+      }
+    });
+  }
 
   private static getMessage(error: any): string {
     if (!!error.error) {
@@ -24,16 +39,31 @@ export class ErrorNotificationService {
 
   handleAPIError<T>(result?: T) {
     return (error: any): Observable<T> => {
-      this.snackBar.openFromComponent(ErrorNotificationComponent,  {
-        duration: 3000,
-        panelClass: ['redSnackBar'],
-        data: {
-          header: 'API Error',
-          message: ErrorNotificationService.getMessage(error)
-        }
-      });
+      this.addToMessageQueue('API Error', ErrorNotificationService.getMessage(error));
       return of(result as T);
     };
   }
 
+  private addToMessageQueue(header: string, message: string): void {
+    const snackBarMessage = new SnackBarMessage(header, message);
+    this.messageQueue.next(snackBarMessage);
+  }
+
+  private openSnackBar(snackBarMessage: SnackBarMessage): void {
+    this.snackBarRef = this.snackBar.openFromComponent(ErrorNotificationComponent, {
+      duration: 3000,
+      panelClass: ['redSnackBar'],
+      data: {
+        header: snackBarMessage.header,
+        message: ErrorNotificationService.getMessage(snackBarMessage.message)
+      }
+    });
+  }
+
+}
+
+class SnackBarMessage {
+  constructor(public header: string,
+              public message: string) {
+  }
 }
