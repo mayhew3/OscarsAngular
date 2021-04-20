992abecbc6a27fe13ce0ba3fc3e5494469891520
Mayhew Seavey <commander.boffo@gmail.com>
2021-04-20 14:13:49 -0700

    Make collapse button work server

--- server/app.ts
+++ server/app.ts
@@ -8,7 +8,9 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../../oscars')));
-
+app.use('/bootstrap_js', express.static(__dirname + '../../../node_modules/bootstrap/dist/js/'));
+app.use('/jquery_js', express.static(__dirname + '../../../node_modules/jquery/dist/'));
+app.use('/popper_js', express.static(__dirname + '../../../node_modules/@popperjs/core/dist/umd/'));
 
 require('./routes.js')(app);
 
