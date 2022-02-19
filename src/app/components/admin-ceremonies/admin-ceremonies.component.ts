37e2c86c22e3004520e9af2d178a21a806bf2ece
Mayhew Seavey <commander.boffo@gmail.com>
2022-02-19 12:10:29 -0800

    MA-216: Basic display

--- src/app/components/admin-ceremonies/admin-ceremonies.component.ts
+++ src/app/components/admin-ceremonies/admin-ceremonies.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CeremonyService} from '../../services/ceremony.service';
+import {PersonService} from '../../services/person.service';
+import {Observable} from 'rxjs';
+import _ from 'underscore';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'osc-admin-ceremonies',
@@ -8,9 +12,32 @@ import {CeremonyService} from '../../services/ceremony.service';
 })
 export class AdminCeremoniesComponent implements OnInit {
 
-  constructor(public ceremonyService: CeremonyService) { }
+
+  constructor(public ceremonyService: CeremonyService,
+              private personService: PersonService) { }
 
   ngOnInit(): void {
   }
 
+  get isAdmin(): boolean {
+    return this.personService.isAdmin;
+  }
+
+  get ceremonyYearsView(): Observable<CeremonyDisplay[]> {
+    return this.ceremonyService.ceremonies.pipe(
+      map(ceremonies => _.flatten(_.map(ceremonies, ceremony => _.map(ceremony.ceremonyYears, ceremonyYear => ({
+            year: ceremonyYear.year,
+            ceremonyName: ceremony.name,
+            numGroups: ceremonyYear.groupYears.length
+          })
+      ))))
+    );
+  }
+
+}
+
+interface CeremonyDisplay {
+  year: number;
+  ceremonyName: string;
+  numGroups: number;
 }
